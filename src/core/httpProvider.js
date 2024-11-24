c10edfb064210305ca8b4b972a5ae723ca02bb78
Mantas Lastakauskas <mantaslastakauskas@wixs-MBP.lan>
2024-11-24 18:33:18 +0200

    Axios http provider added

---
+++ src/core/httpProvider.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const httpProvider = {
+  get: (url) => axiosInstance.get(url),
+  post: (url, data) => axiosInstance.post(url, data),
+  put: (url, data) => axiosInstance.put(url, data),
+  delete: (url) => axiosInstance.delete(url),
+};
--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import "./style.css";
 import App from "./App.vue";
+import { httpProvider } from "@/core/httpProvider";
 
 const pinia = createPinia();
 const app = createApp(App);
+
+httpProvider.get("/data.json").then((data) => {
+  console.log("data", data);
+});
 
 app.use(pinia);
 app.mount("#app");
